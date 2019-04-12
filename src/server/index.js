5db767b6930fc17f7ce45611ebc6282542243705
charlie <cgarzon132@gmail.com>
2019-04-12 11:42:39 -0500

    koa router addition

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,10 @@
 const Koa = require('koa')
+const indexRoutes = require('./routes/index')
 
 const app = new Koa()
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
-app.use(async (ctx) => {
-  ctx.body = {
-    status: 'success',
-    message: 'hello, world!'
-  }
-})
+app.use(indexRoutes.routes())
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`)
